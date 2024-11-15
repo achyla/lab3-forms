84fe8a78d35d3657625c85a3828b858b5f87ab22
achyla <148362165+achyla@users.noreply.github.com>
2024-11-15 20:22:59 +0100

    Upload

---
+++ Controllers/DaneController.cs
@@ -0,0 +1,34 @@
+﻿using lab3_forms.Models;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+namespace lab3_forms.Controllers
+{
+    public class DaneController : Controller
+    {
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form(Dane dane)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Wynik", dane);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Wynik(Dane dane)
+        {
+            return View(dane);
+        }
+    }
+}
---
+++ Models/Dane.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lab3_forms.Models
+{
+    public class Dane
+    {
+        [Required(ErrorMessage = "Proszę podać imię")]
+        [MinLength(2, ErrorMessage = "Imię musi zawierać minimum 2 znaki")]
+        public string Imie { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać nazwisko")]
+        [MinLength(2, ErrorMessage = "Nazwisko musi zawierać minimum 2 znaki")]
+        public string Nazwisko { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać adres e-mail")]
+        [EmailAddress(ErrorMessage = "Nieprawidłowy format adresu e-mail")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać hasło")]
+        [MinLength(8, ErrorMessage = "Hasło musi mieć co najmniej 8 znaków")]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$", ErrorMessage = "Hasło musi zawierać co najmniej jedną dużą literę, jedną małą literę i jedną cyfrę")]
+        [DataType(DataType.Password)]
+        public string Haslo { get; set; }
+
+        [Required(ErrorMessage = "Proszę potwierdzić hasło")]
+        [Compare("Haslo", ErrorMessage = "Hasła nie są zgodne")]
+        [DataType(DataType.Password)]
+        public string PotwierdzenieHasla { get; set; }
+
+        [Phone(ErrorMessage = "Proszę podać poprawny numer telefonu")]
+        [DataType(DataType.PhoneNumber)]
+        public string NrTelefonu { get; set; }
+
+        [Range(10, 80, ErrorMessage = "Wiek musi być między 10 a 80 lat")]
+        public int Wiek { get; set; }
+
+        [Required(ErrorMessage = "Proszę wybrać miasto")]
+        public Miasta Miasto { get; set; }
+
+        public enum Miasta
+        {
+            Warszawa,
+            Krakow,
+            Gdansk,
+            Wroclaw,
+            Poznan
+        }
+    }
+}
